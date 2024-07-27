8a2ef494c4c906113ae066e874bed86192031a29
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-27 19:05:41 +0300

    add simple form validation and confirmation

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,13 +1,36 @@
 const form = document.getElementById('contact-us-form');
+const confirmation = document.getElementById('contact-us-confirm');
+const elements = form.elements;
+const placeholders = {};
+for (let i = 0; i < elements.length; i++) {
+  const element = elements[i];
+  placeholders[element.name] = element.placeholder;
+}
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  const elements = form.elements;
   const data = {};
+  let error = false;
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
+    if (element.nodeName !== 'INPUT') {
+      continue;
+    }
+    // put your validation here
+    if (!element.value || element.value.trim() === '') {
+      error = true;
+      element.classList.add('error');
+      element.placeholder = 'This is a required field.';
+    } else {
+      element.classList.remove('error');
+      element.placeholder = placeholders[element.name];
+    }
     data[element.name] = element.value;
   }
-  console.log(data);
-  form.reset();
+  if (!error) {
+    console.log(data);
+    form.reset();
+    form.style.display = 'none';
+    confirmation.style.display = 'block';
+  }
 });
