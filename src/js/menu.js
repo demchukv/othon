f23c521f714cd9a84a3cc44e23673f57fcfc99e6
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-28 15:03:48 +0300

    add mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -14,12 +14,17 @@ openBtn.addEventListener('click', () => openMenu());
 closeBtn.addEventListener('click', () => closeMenu());
 
 const openMenu = () => {
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  // const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
+
   head.classList.add('open');
   logo.style.fill = '#ffffff';
   openBtn.classList.add('hidden');
   closeBtn.classList.remove('hidden');
   menuBg.classList.add('open');
-  body.classList.add('modal-open');
+  // body.classList.add('modal-open');
   setTimeout(() => {
     menu.classList.add('open');
     contactBtn.classList.add('open');
@@ -27,12 +32,23 @@ const openMenu = () => {
 };
 
 const closeMenu = () => {
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
   head.classList.remove('open');
   logo.style.fill = '';
   openBtn.classList.remove('hidden');
   closeBtn.classList.add('hidden');
   menuBg.classList.remove('open');
-  body.classList.remove('modal-open');
+  // body.classList.remove('modal-open');
   menu.classList.remove('open');
   contactBtn.classList.remove('open');
 };
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty(
+    '--scroll-y',
+    `${window.scrollY}px`
+  );
+});
