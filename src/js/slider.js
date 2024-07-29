218e7a15c871e3e9b94ba24fd24f819527eebc0c
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-29 12:35:36 +0300

    services section for mobile

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,40 +1,102 @@
-const sItems = document.querySelectorAll('.services-info__item');
-const sSlides = document.querySelectorAll('.services-info__slide');
-const sText = document.querySelectorAll('.services-info__text-item');
-let sCurrent = 0;
+function updateSize() {
+  let width = document.documentElement.clientWidth;
+  // for desktop
+  if (width > 768) {
+    const sItems = document.querySelectorAll('.services-info__item');
+    const sSlides = document.querySelectorAll('.services-info__slide');
+    const sText = document.querySelectorAll('.services-info__text-item');
+    let sCurrent = 0;
 
-sItems.forEach((item, index) => {
-  item.addEventListener('click', e => {
-    e.preventDefault();
+    sItems.forEach((item, index) => {
+      item.addEventListener('click', e => {
+        e.preventDefault();
 
-    sItems[sCurrent].classList.remove('item-active');
-    sItems[index].classList.add('item-active');
-    sText[sCurrent].classList.remove('text-active');
-    sText[index].classList.add('text-active');
+        sItems[sCurrent].classList.remove('item-active');
+        sItems[index].classList.add('item-active');
+        sText[sCurrent].classList.remove('text-active');
+        sText[index].classList.add('text-active');
 
-    sSlides[sCurrent].style.zIndex = '1';
-    sSlides[index].style.zIndex = '2';
-    if (sCurrent < index) {
-      sSlides[index].animate(
-        [{ transform: 'translateX(-100%)' }, { transform: 'translateX(0)' }],
-        {
-          duration: 400,
-        },
-      );
-    }
-    if (sCurrent > index) {
-      sSlides[index].animate(
-        [{ transform: 'translateX(100%)' }, { transform: 'translateX(0)' }],
-        {
-          duration: 400,
-        },
-      );
-    }
-    setTimeout(() => {
-      sSlides[sCurrent].classList.remove('slide-active');
-      sSlides[index].classList.add('slide-active');
-      sSlides[sCurrent].style.zIndex = '0';
+        sSlides[sCurrent].style.zIndex = '1';
+        sSlides[index].style.zIndex = '2';
+        if (sCurrent < index) {
+          sSlides[index].animate(
+            [
+              { transform: 'translateX(-100%)' },
+              { transform: 'translateX(0)' },
+            ],
+            {
+              duration: 400,
+            }
+          );
+        }
+        if (sCurrent > index) {
+          sSlides[index].animate(
+            [{ transform: 'translateX(100%)' }, { transform: 'translateX(0)' }],
+            {
+              duration: 400,
+            }
+          );
+        }
+        setTimeout(() => {
+          sSlides[sCurrent].classList.remove('slide-active');
+          sSlides[index].classList.add('slide-active');
+          sSlides[sCurrent].style.zIndex = '0';
+          sCurrent = index;
+        }, 500);
+      });
+    });
+
+    // for mobile
+  } else {
+    const prev = document.getElementById('prev-slide');
+    const next = document.getElementById('next-slide');
+    const sItems = document.querySelectorAll('.services-info__item');
+    const sSlides = document.querySelectorAll('.services-info__slide');
+    const sText = document.querySelectorAll('.services-info__text-item');
+    let sCurrent = 0;
+    let index = 0;
+    const total = sItems.length;
+
+    sSlides.forEach((item, index) => {
+      sText[
+        index
+      ].style.background = `linear-gradient(180deg, #18306c 0%, rgba(47, 93, 210, 0) 100%),     url(${item.children[0].src}) no-repeat center / cover`;
+    });
+
+    prev.addEventListener('click', () => {
+      if (sCurrent > 0) {
+        index = sCurrent - 1;
+      } else {
+        index = total - 1;
+      }
+      setSlide(sCurrent, index);
       sCurrent = index;
-    }, 500);
-  });
-});
+    });
+    next.addEventListener('click', () => {
+      if (sCurrent < total - 1) {
+        index = sCurrent + 1;
+      } else {
+        index = 0;
+      }
+      setSlide(sCurrent, index);
+      sCurrent = index;
+    });
+  }
+}
+
+function setSlide(sCurrent, index) {
+  const sItems = document.querySelectorAll('.services-info__item');
+  const sSlides = document.querySelectorAll('.services-info__slide');
+  const sText = document.querySelectorAll('.services-info__text-item');
+
+  sItems[sCurrent].classList.remove('item-active');
+  sItems[index].classList.add('item-active');
+  sText[sCurrent].classList.remove('text-active');
+  sText[index].classList.add('text-active');
+  // sText[index].animate([{ opacity: 1 }, { opacity: 0 }], {
+  //   duration: 400,
+  // });
+}
+
+updateSize();
+window.addEventListener('resize', updateSize);
